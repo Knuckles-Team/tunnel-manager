1b2b442e81d6ae25dc68f8f5bab090da43f8568c
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-06 01:37:13 -0600

    Bump version: 1.1.27 → 1.1.28

--- tunnel_manager/agent.py
+++ tunnel_manager/agent.py
@@ -11,7 +11,7 @@ from agent_utilities import (
     load_identity,
 )
 
-__version__ = "1.1.27"
+__version__ = "1.1.28"
 
 logging.basicConfig(
     level=logging.INFO,
--- tunnel_manager/tunnel_manager.py
+++ tunnel_manager/tunnel_manager.py
@@ -9,7 +9,7 @@ import os
 import paramiko
 import yaml
 
-__version__ = "1.1.27"
+__version__ = "1.1.28"
 
 
 class HostManager:
