475e121c78d927503d96b769855ecb8345ac84d0
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-13 22:24:13 -0500

    Bump version: 1.1.40 → 1.1.41

--- tunnel_manager/mcp_server.py
+++ tunnel_manager/mcp_server.py
@@ -21,7 +21,7 @@ from agent_utilities.mcp_utilities import (
     config,
 )
 
-__version__ = "1.1.40"
+__version__ = "1.1.41"
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
